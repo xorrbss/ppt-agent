2ea3680c236847db0db23bafcc623469d8cadfbc
sauravniraula <developmentsaurav@gmail.com>
2025-09-16 17:33:34 +0545

    feat(fastapi): adds mock endpoints for webhook events

--- servers/fastapi/api/main.py
+++ servers/fastapi/api/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.lifespan import app_lifespan
 from api.middlewares import UserConfigEnvUpdateMiddleware
+from api.v1.mock.router import API_V1_MOCK_ROUTER
 from api.v1.ppt.router import API_V1_PPT_ROUTER
 
 
@@ -10,6 +11,7 @@ app = FastAPI(lifespan=app_lifespan)
 
 # Routers
 app.include_router(API_V1_PPT_ROUTER)
+app.include_router(API_V1_MOCK_ROUTER)
 
 # Middlewares
 origins = ["*"]
